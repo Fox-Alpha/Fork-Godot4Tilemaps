c2883b941aa663a6d397ed8f5a2227abda29cc9d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-03-12 21:21:03 +0100

    Fix: Debugger Warnings about float division

--- scenes/world/world_boundarys.gd
+++ scenes/world/world_boundarys.gd
@@ -11,7 +11,7 @@ func ResetWorldBorderPositions(maprect : Vector2i) -> void:
 	if worldborders.size() != 4:
 		return
 
-	worldborders[0].position.x = -(maprect.x / 2) + 32	#-2000		#Left	+2Tiles
-	worldborders[1].position.y = (-maprect.y / 2) +32	#-4400		#Top
-	worldborders[2].position.x = (maprect.x / 2) - 32	#2000		#Right
-	worldborders[3].position.y = (maprect.y / 2) -32	#4400		#Bottom
+	worldborders[0].position.x = -(maprect.x / 2.0) + 32	#-2000		#Left	+2Tiles
+	worldborders[1].position.y = (-maprect.y / 2.0) +32	#-4400		#Top
+	worldborders[2].position.x = (maprect.x / 2.0) - 32	#2000		#Right
+	worldborders[3].position.y = (maprect.y / 2.0) -32	#4400		#Bottom
