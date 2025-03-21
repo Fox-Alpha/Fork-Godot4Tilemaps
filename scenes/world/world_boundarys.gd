c9a4edfe2d67fa0de92e5444003ff5879d6f7732
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-03-21 23:40:13 +0100

    Fix: setting Map Boundarys

--- scenes/world/world_boundarys.gd
+++ scenes/world/world_boundarys.gd
@@ -7,11 +7,12 @@ func _ready() -> void:
 	pass
 
 
-func ResetWorldBorderPositions(maprect : Vector2i) -> void:
+func ResetWorldBorderPositions(maprect : Vector2i, _l) -> void:
 	if worldborders.size() != 4:
 		return
 
-	worldborders[0].position.x = -(maprect.x / 2.0)	#-2000		#Left	+2Tiles
-	worldborders[1].position.y = (-maprect.y / 2.0)	#-4400		#Top
-	worldborders[2].position.x = (maprect.x / 2.0) 	#2000		#Right
-	worldborders[3].position.y = (maprect.y / 2.0) 	#4400		#Bottom
+	worldborders[0].position.x = 0 #-(maprect.x / 2.0)	#-2000		#Left	+2Tiles
+	worldborders[1].position.y = 0 #(-maprect.y / 2.0)	#-4400		#Top
+	worldborders[2].position.x = maprect.x #(maprect.x / 2.0) 	#2000		#Right
+	worldborders[3].position.y = maprect.y #(maprect.y / 2.0) 	#4400		#Bottom
+	pass
