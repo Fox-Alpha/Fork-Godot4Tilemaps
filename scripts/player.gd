8935ced4e50dae256c99160a938c534ae06dd053
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-03-21 23:37:10 +0100

    Add: Reimplement Player Respawn after generating

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
 
+@export var tile_map_layers : Array[TileMapLayer] = []
+enum LAYERS {
+	ground_layer_0 = 0,
+	ground_layer_1 = 1,
+	ground_layer_2 = 2,
+	cliff_layer = 3,
+	environment_layer =4	
+}
 
 @export var SPEED : float = 300.0
 
@@ -7,10 +15,12 @@ extends CharacterBody2D
 var direction : Vector2 = Vector2.ZERO
 
 func _ready():
+	GlobalSignalBus.World_Generated.connect(ResetPlayerPosition)
 	animation_tree.active = true
 
 func _process(_delta):
 	update_animation_parameters()
+
 
 func _physics_process(_delta):
 	direction = Input.get_vector("left", "right","up","down").normalized()
@@ -32,4 +42,27 @@ func update_animation_parameters():
 	if direction != Vector2.ZERO:
 		animation_tree["parameters/idle/blend_position"] = direction
 		animation_tree["parameters/walk/blend_position"] = direction
+
+
+func ResetPlayerPosition(_mapsize, layer) -> void:
+	var tml : TileMapLayer = get_node_or_null("/root/World/LayerGroup").get_children(layer)[0] as TileMapLayer
+	var newtile := Vector2i.ZERO 
+	var nst : Array[Vector2i] = [] 
+	var test_tile : Array[Vector2i] = []
+
+	var arr : Array = tml.get_used_cells()
+
+	while test_tile.size() != 4 and arr.size() > 0:
+		newtile = arr.pick_random()
+		nst = tml.get_surrounding_cells(newtile)
+
+		test_tile = nst.filter(func(coord): 
+			var atl_coords = tml.get_cell_atlas_coords(coord)
+			return atl_coords != Vector2i(0,1))
+
+	var gpp  = tml.to_global(tml.map_to_local(newtile))
+	global_position = gpp
 	
+	print("Respawn Player Position %s / %s " % [gpp, newtile])
+	GlobalSignalBus.PLayer_Respawned.emit(gpp)
+	pass
