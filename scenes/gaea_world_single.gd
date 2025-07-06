0a1c0d3a1cf9cb4c363cd9e2442d9b5d6890760c
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-06 22:21:57 +0200

    Fix: was used wron generation_finished signal to get generated map size

--- scenes/gaea_world_single.gd
+++ scenes/gaea_world_single.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 @onready var LayerGroupNode : Node = get_node_or_null("/root/World/LayerGroup")
-#@onready var tilemap_gaea_renderer: TilemapGaeaRenderer = $TilemapGaeaRenderer
-@onready var threaded_tilemap_gaea_renderer: ThreadedTilemapGaeaRenderer = $ThreadedTilemapGaeaRenderer
 
 
 var tile_map_layers : Array[TileMapLayer] = []
@@ -14,14 +12,17 @@ var tile_map_layers : Array[TileMapLayer] = []
 	#environment_layer =4	
 #}
 # Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
 	if LayerGroupNode:
-		#tile_map_layers.append_array(LayerGroupNode.get_children())
-		tile_map_layers.append_array(threaded_tilemap_gaea_renderer.tile_map_layers)
-		#tilemap_gaea_renderer.tile_map_layers.append_array(LayerGroupNode.get_children())
+		tile_map_layers.append_array(LayerGroupNode.get_children())
 	pass # Replace with function body.
 
 
-func _on_noise_generator_generation_finished() -> void:
-	var mapsize := tile_map_layers[0].get_used_rect().size * tile_map_layers[0].tile_set.tile_size
-	GlobalSignalBus.World_Generated.emit(mapsize, 0)
+func _on_heightmap_generator_2d_generation_finished() -> void:
+	if LayerGroupNode and tile_map_layers.size() > 0:
+		var tml : TileMapLayer = tile_map_layers[0]
+		var mapsize := tml.get_used_rect().size * tml.tile_set.tile_size
+		GlobalSignalBus.World_Generated.emit(mapsize, 0)
+	pass # Replace with function body.
