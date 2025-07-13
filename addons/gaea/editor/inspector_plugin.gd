288104d0bf04f46e2ba93bc2f64bf76dc006c72f
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-13 14:39:27 +0200

    Upd: GAEA Version updated

--- addons/gaea/editor/inspector_plugin.gd
+++ addons/gaea/editor/inspector_plugin.gd
@@ -1,33 +1,19 @@
 extends EditorInspectorPlugin
 
 
+const GradientVisualizer = preload("res://addons/gaea/editor/gradient_visualizer.gd")
+
+
 func _can_handle(object: Object) -> bool:
-	return object is GaeaGenerator or object is Modifier2D or object is Modifier3D or object is NoiseGeneratorData or object is NoiseCondition
+	return object is GradientGaeaMaterial
 
 
 func _parse_begin(object: Object) -> void:
-	if object is GaeaGenerator:
-		var generator_buttons := preload("./generator_buttons.gd").new()
-		add_custom_control(generator_buttons)
+	if object is GradientGaeaMaterial:
+		var gradient_visualizer := GradientVisualizer.new()
+		gradient_visualizer.gradient = object
 
-	if object is Modifier or object is NoiseCondition:
-		if not object.get("noise"):
-			return
+		add_custom_control(gradient_visualizer)
 
-		if not object.get("min") and not object.get("max"):
-			return
-	elif object is NoiseGeneratorData:
-		if not object.settings:
-			return
-	else:
-		return
-
-	var texture_rect := preload("./threshold_visualizer.gd").new()
-	texture_rect.object = object
-
-	add_custom_control(texture_rect)
-
-	texture_rect.update()
-	if object.get("noise"):
-		object.get("noise").changed.connect(texture_rect.update)
-	object.changed.connect(texture_rect.update)
+		gradient_visualizer.update()
+		object.points_sorted.connect(gradient_visualizer.update)
