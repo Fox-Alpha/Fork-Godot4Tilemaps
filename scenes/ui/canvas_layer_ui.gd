695086311c7659ad8d67d8452195e2802a33e1c7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-24 23:21:37 +0200

    Mve: input for building keys to global Fix / Chg: output in position laben

--- scenes/ui/canvas_layer_ui.gd
+++ scenes/ui/canvas_layer_ui.gd
@@ -10,22 +10,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var GlMoPos := get_global_mouse_position()
 	var tml : TileMapLayer = ground_tile_layer
-	var loc_mouse_coord : Vector2i = tml.local_to_map(GlMoPos)
-	text = "Mouse Position in Map: %s" % str(loc_mouse_coord)
+	var tml_lmp := tml.get_local_mouse_position()
+	text = "Mouse Position in Map: Tile on Map: %s" % [
+		str(tml.local_to_map(tml_lmp))
+	]
 
 
-func _unhandled_key_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
-		match event.keycode:
-			KEY_1:
-				print("Key 1 verwendet: Command Center")
-			KEY_2:
-				print("Key 2 verwendet: Powerplant")
-			KEY_3:
-				print("Key 3 verwendet: Refinery")
-			KEY_4:
-				print("Key 4 verwendet: Barracks")
-			KEY_5:
-				print("Key 5 verwendet: Radar")
+#func _unhandled_key_input(event: InputEvent) -> void:
+	#if event is InputEventKey and event.is_pressed():
+		#match event.keycode:
+			#KEY_1:
+				#print("Key 1 verwendet: Command Center")
+			#KEY_2:
+				#print("Key 2 verwendet: Powerplant")
+			#KEY_3:
+				#print("Key 3 verwendet: Refinery")
+			#KEY_4:
+				#print("Key 4 verwendet: Barracks")
+			#KEY_5:
+				#print("Key 5 verwendet: Radar")
--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -15,3 +15,18 @@ func _init() -> void:
 
 func _ready() -> void:
 	print("Globals => _ready()")
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		match event.keycode:
+			KEY_1:
+				print("Key 1 verwendet: Command Center")
+			KEY_2:
+				print("Key 2 verwendet: Powerplant")
+			KEY_3:
+				print("Key 3 verwendet: Refinery")
+			KEY_4:
+				print("Key 4 verwendet: Barracks")
+			KEY_5:
+				print("Key 5 verwendet: Radar")
