807c3d0a0d2730560b5625e9b475890004471a2a
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-24 21:48:45 +0200

    Add: Dummy Signal func Add: Doc Comments

--- scripts/globals/signal_bus.gd
+++ scripts/globals/signal_bus.gd
@@ -1,4 +1,15 @@
-class_name SignalBus extends Node
+## Globale Signalsammlung, persistent
+class_name GlobalSignalBus extends Node
 
+## Ausgelöst wenn die Kartegernerierung beendet ist
 signal World_Generated
+
+## Ausgelöst wenn der Spieler auf dem Spielfeld platziert wurde
 signal PLayer_Respawned
+
+
+func _ready() -> void:
+	print("GlobalSignalBus => _ready()")
+
+	World_Generated.connect(func(): pass )
+	PLayer_Respawned.connect(func(): pass )
