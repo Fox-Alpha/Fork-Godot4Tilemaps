22eee5ae8ebc5a84b066d0caef6e9a7f9dd1fe8d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-26 02:27:14 +0200

    Add: Layerscript to place Building on mouseclick

---
+++ scripts/building_tile_layer.gd
@@ -0,0 +1,31 @@
+extends TileMapLayer
+
+@export var NavigationLayer : TileMapLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if !is_instance_valid(GlobalVars.BuildingToPlace) or !GlobalVars.BuildingMode:
+				return
+			
+			var mouse_pos = get_global_mouse_position()
+			var tile_coords = local_to_map(to_local(mouse_pos))
+			var instance = GlobalVars.BuildingToPlace
+			instance.position = map_to_local(tile_coords)
+			add_child(instance)
+			instance.owner = self
+			GlobalVars.BuildingMode = false
+			NavigationLayer.AddSolidTilesFromPlacedBuilding(tile_coords)
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed and GlobalVars.BuildingMode:
+			GlobalVars.BuildingToPlace.queue_free()
+			GlobalVars.BuildingMode = false
