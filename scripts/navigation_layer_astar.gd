115fafb462032a9106259197380ebf9c194394b4
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-27 00:31:28 +0200

    Chg: start astar navigation to RMB

--- scripts/navigation_layer_astar.gd
+++ scripts/navigation_layer_astar.gd
@@ -27,7 +27,7 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+		if event.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
 			if GlobalVars.BuildingMode:
 				return
 
