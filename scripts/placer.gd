cfabd98fe92f7d5b72f1776297f25c86d05b9da9
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-27 04:03:38 +0200

    Chg: Replacing Building Constance with Dictionary Add: Handling LMB for Placing. Emit Signal

--- scripts/placer.gd
+++ scripts/placer.gd
@@ -2,11 +2,29 @@ extends Node2D
 
 @onready var build_ghost: Sprite2D = get_node_or_null("BuildGhost")
 
-const BARRACKS = preload("uid://dijgpdkai6m20")
-const COMMAND_CENTER = preload("uid://c8q5s3mdfbhp0")
-const RADAR = preload("uid://dryt1143aijbt")
-const REFINERY = preload("uid://bm40tsnfhlvxc")
-const POWERPLANT = preload("uid://soue6jtn4lpw")
+const Buildings : Dictionary = {
+	"BARRACKS" : {
+		"Path" : "uid://dijgpdkai6m20",
+		"Image" : "res://assets/buildings/barracks.png"
+		},
+	"COMMAND_CENTER" : {
+		"Path":"uid://c8q5s3mdfbhp0",
+		"Image" : "res://assets/buildings/commandcenter.png"
+		},
+	"RADAR" : {
+		"Path":"uid://dryt1143aijbt",
+		"Image" : "res://assets/buildings/radar.png"
+		},
+	"REFINERY" : {
+		"Path":"uid://bm40tsnfhlvxc",
+		"Image" : "res://assets/buildings/refinery.png"
+		},
+	"POWERPLANT" : {
+		"Path":"uid://soue6jtn4lpw",
+		"Image" : "res://assets/buildings/powerplant.png"
+		},
+}
+var BuildingToPlace : String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,21 +39,24 @@ func _process(_delta: float) -> void:
 
 
 func EnterBuildMode() -> void:
-	if GlobalVars.BuildingMode and is_instance_valid(GlobalVars.BuildingToPlace):
-		var BuildImage : Texture2D = GlobalVars.BuildingToPlace.get_node("Picture").texture
-		build_ghost.texture = BuildImage as Texture2D
+	if GlobalVars.BuildingMode:
+		var BuildImage : Image = Image.load_from_file(Buildings[BuildingToPlace].Image)
+		#GlobalVars.BuildingToPlace.get_node("Picture").texture
+		var texture = ImageTexture.create_from_image(BuildImage)
+
+		build_ghost.texture = texture
 		build_ghost.show()
 	pass
 
 func LeaveBuildMode() -> void:
 	build_ghost.texture = null
-	GlobalVars.BuildingToPlace = null
+	BuildingToPlace = ""
 	build_ghost.hide()
 	pass
 
 
 func HandleBuildMode():
-	if GlobalVars.BuildingMode and is_instance_valid(GlobalVars.BuildingToPlace):
+	if GlobalVars.BuildingMode:
 		#global_position = GlobalVars.GlobalMousePosition
 		pass
 
@@ -43,19 +64,23 @@ func HandleBuildMode():
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		global_position = GlobalVars.GlobalMousePosition
+		#TODO: Check Waterlayer, if placeable
 
 	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			pass
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			pass
+		if GlobalVars.BuildingMode:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				var instance = load(Buildings[BuildingToPlace].Path).instantiate()
+				GlobalVars.GSB.Building_Structure_Placed.emit(instance.get_instance_id(), get_global_mouse_position())
+				pass
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				GlobalVars.BuildingMode = false
+				get_viewport().set_input_as_handled()
+				pass
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
 		GlobalVars.BuildingMode = checkBuildingKey(event.keycode)
-		if GlobalVars.BuildingMode:
-			GlobalVars.GSB.Enter_Buildmode.emit()
 		pass
 
 
@@ -64,28 +89,25 @@ func checkBuildingKey(keycode : Key) -> bool:
 	match keycode:
 		KEY_1:
 			print("Key 1 verwendet: Command Center")
-			GlobalVars.BuildingToPlace = COMMAND_CENTER.instantiate()
+			BuildingToPlace = "COMMAND_CENTER"
 			return true
 		KEY_2:
 			print("Key 2 verwendet: Powerplant")
-			GlobalVars.BuildingToPlace = POWERPLANT.instantiate()
+			BuildingToPlace = "POWERPLANT"
 			return true
 		KEY_3:
 			print("Key 3 verwendet: Refinery")
-			GlobalVars.BuildingToPlace = REFINERY.instantiate()
+			BuildingToPlace = "REFINERY"
 			return true
 		KEY_4:
 			print("Key 4 verwendet: Barracks")
-			GlobalVars.BuildingToPlace = BARRACKS.instantiate()
+			BuildingToPlace = "BARRACKS"
 			return true
 		KEY_5:
 			print("Key 5 verwendet: Radar")
-			GlobalVars.BuildingToPlace = RADAR.instantiate()
+			BuildingToPlace = "RADAR"
 			return true
 		KEY_ESCAPE:
-			if not GlobalVars.BuildingToPlace.is_queued_for_deletion():
-			#if is_instance_valid(BuildingToPlace):
-				GlobalVars.BuildingToPlace.queue_free()
 			return false
 		_:
 			return false
