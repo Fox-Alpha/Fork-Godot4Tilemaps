8e9feb1f35c6cdbe2b492214dc1c88926679740a
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-27 22:43:58 +0200

    Chg: extends from TileMapLayerExtension Add: set astar instrance Chg: call to parent class func AddTileRectToMap ===: Name changed from AddSolidTilesFromPlacedBuilding

--- scripts/navigation_layer_astar.gd
+++ scripts/navigation_layer_astar.gd
@@ -1,4 +1,4 @@
-extends TileMapLayer
+extends TileMapLayerExtension
 
 @onready var path_line_2d: Line2D = $"../AStarPathLine"
 
@@ -13,11 +13,13 @@ func _ready() -> void:
 	GlobalVars.GSB.World_Generated.connect(SetNavigationLayer)
 	GlobalVars.GSB.Building_Structure_Placed.connect(
 		func(_iid:int, coords:Vector2i):
-			AddSolidTilesFromPlacedBuilding(local_to_map(to_local(coords)))
+			AddTileRectToMap(local_to_map(to_local(coords)), 3, true)
 			)
+	_astar = astar_grid
 	pass # Replace with function body.
 
-#region unusef prebuild funcs
+
+#region unused prebuild funcs
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(_delta: float) -> void:
 	#pass
@@ -45,6 +47,8 @@ func _unhandled_input(event: InputEvent) -> void:
 					GlobalVars.astarpath.append(to_global(map_to_local(idpath[i])) as Vector2i)
 			idpath.clear()
 		pass
+	pass
+
 
 ## 
 func SetNavigationLayer(_LayerSize : Vector2i, _l) -> void:
@@ -62,31 +66,4 @@ func SetNavigationLayer(_LayerSize : Vector2i, _l) -> void:
 	# set solid tiles
 	for s in guc_necp.size():
 		astar_grid.set_point_solid(guc_necp[s])
-
-	pass
-
-
-func get_empty_cell_positions_in_rect(rect2: Rect2, returnnotemptytiles : bool = true) -> Array[Vector2i]:
-	var empty_cell_positions : Array[Vector2i] = []
-
-	for y in rect2.size.y:
-		for x in rect2.size.x:
-			var cell_pos : Vector2i = (rect2.position + Vector2(x, y)) as Vector2i
-			if returnnotemptytiles:
-				if get_cell_atlas_coords(cell_pos) != Vector2i(-1,-1):
-					empty_cell_positions.append(cell_pos)
-			else:
-				if get_cell_atlas_coords(cell_pos) == Vector2i(-1,-1):
-					empty_cell_positions.append(cell_pos)
-	return empty_cell_positions
-
-
-func AddSolidTilesFromPlacedBuilding(BuildingPosition : Vector2i) -> void:
-	var BuildingRectStart : Vector2i = Vector2i(BuildingPosition.x-3, BuildingPosition.y-3)
-
-	for y in range(7):
-		for x in range(7):
-			set_cell(Vector2i(BuildingRectStart.x + x, BuildingRectStart.y + y), 0, Vector2i(11, 0))
-			astar_grid.set_point_solid(Vector2i(BuildingRectStart.x + x, BuildingRectStart.y + y))
-			pass
 	pass
