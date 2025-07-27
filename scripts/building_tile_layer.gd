8b2877e3d4cae9573fba376a8dc745871e3f1b90
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-27 04:00:15 +0200

    Rem: MouseButton Handling for Instantiate and placeing Building Ad. Reaction to Placing Signal

--- scripts/building_tile_layer.gd
+++ scripts/building_tile_layer.gd
@@ -4,6 +4,16 @@ extends TileMapLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	GlobalVars.GSB.Building_Structure_Placed.connect(
+		func(iid:int, coords:Vector2i):
+			var instance = instance_from_id(iid)
+			if is_instance_valid(instance):
+				instance.position = to_local(coords)
+				add_child(instance)
+				instance.owner = self
+				GlobalVars.BuildingMode = false
+			pass
+	)
 	pass # Replace with function body.
 
 
@@ -15,16 +25,10 @@ func _process(_delta: float) -> void:
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if !is_instance_valid(GlobalVars.BuildingToPlace) or !GlobalVars.BuildingMode:
-				return
-			
-			var mouse_pos = get_global_mouse_position()
-			var tile_coords = local_to_map(to_local(mouse_pos))
-			var instance = GlobalVars.BuildingToPlace
-			instance.position = map_to_local(tile_coords)
-			add_child(instance)
-			instance.owner = self
-			GlobalVars.BuildingMode = false
-			NavigationLayer.AddSolidTilesFromPlacedBuilding(tile_coords)
-		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed and GlobalVars.BuildingMode:
-			GlobalVars.BuildingMode = false
+			pass
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			pass
+
+
+func _child_entered_tree(_node : Node) -> void:
+	pass
