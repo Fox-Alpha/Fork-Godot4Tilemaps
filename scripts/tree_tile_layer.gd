4ba925ed742ec51d1814cf65b828402666decdc7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-07-27 22:37:53 +0200

    Mve: AddSolidTilesFromPlacedBuilding to parent extension class

--- scripts/tree_tile_layer.gd
+++ scripts/tree_tile_layer.gd
@@ -1,11 +1,11 @@
-extends TileMapLayer
+extends TileMapLayerExtension
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalVars.GSB.Building_Structure_Placed.connect(
 		func(_iid:int, coords:Vector2i):
-			AddSolidTilesFromPlacedBuilding(local_to_map(to_local(coords)))
+			AddTileRectToMap(local_to_map(to_local(coords)), 3)
 			)
 	pass # Replace with function body.
 
@@ -13,14 +13,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#pass
-
-
-func AddSolidTilesFromPlacedBuilding(BuildingPosition : Vector2i) -> void:
-	var BuildingRectStart : Vector2i = Vector2i(BuildingPosition.x-3, BuildingPosition.y-3)
-
-	for y in range(7):
-		for x in range(7):
-			set_cell(Vector2i(BuildingRectStart.x + x, BuildingRectStart.y + y), 0, Vector2i(-1, -1))
-			
-			pass
-	pass
