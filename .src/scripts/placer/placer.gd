190ce2db910aadc3cb05a3d048b73bff5386b5e9
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-01 22:34:56 +0200

    Mve: Building dictionary to BuildingManager Mve: Building selection Key Input to BuildingManager Add: new signal receiver building_placement_possible

--- .src/scripts/placer/placer.gd
+++ .src/scripts/placer/placer.gd
@@ -2,39 +2,34 @@ extends Node2D
 
 @onready var build_ghost: Sprite2D = get_node_or_null("BuildGhost")
 
-const Buildings : Dictionary = {
-	"BARRACKS" : {
-		"Path" : "uid://dijgpdkai6m20",
-		"Image" : "res://assets/buildings/barracks.png"
-		},
-	"COMMAND_CENTER" : {
-		"Path":"uid://c8q5s3mdfbhp0",
-		"Image" : "res://assets/buildings/commandcenter.png"
-		},
-	"RADAR" : {
-		"Path":"uid://dryt1143aijbt",
-		"Image" : "res://assets/buildings/radar.png"
-		},
-	"REFINERY" : {
-		"Path":"uid://bm40tsnfhlvxc",
-		"Image" : "res://assets/buildings/refinery.png"
-		},
-	"POWERPLANT" : {
-		"Path":"uid://soue6jtn4lpw",
-		"Image" : "res://assets/buildings/powerplant.png"
-		},
-}
-var BuildingToPlace : String = ""
+var placement_possible : bool = true :
+	set(value):
+		if placement_possible != value:
+			placement_possible = value
+	get():
+		return placement_possible
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalVars.GSB.Enter_Buildmode.connect(EnterBuildMode)
 	GlobalVars.GSB.Leave_Buildmode.connect(LeaveBuildMode)
+	GlobalVars.GSB.Building_Placement_Possible.connect(
+	func(_possible : bool):
+		placement_possible = _possible
+		pass
+	)
 	pass # Replace with function body.
 
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	HandleBuildMode()
+	pass
+
+
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -48,6 +43,7 @@ func EnterBuildMode() -> void:
 		build_ghost.show()
 	pass
 
+
 func LeaveBuildMode() -> void:
 	build_ghost.texture = null
 	BuildingToPlace = ""
@@ -56,7 +52,7 @@ func LeaveBuildMode() -> void:
 
 
 func HandleBuildMode():
-	if GlobalVars.BuildingMode:
+	if GlobalVars.GetBuildingMode():
 		#global_position = GlobalVars.GlobalMousePosition
 		pass
 
@@ -64,7 +60,7 @@ func HandleBuildMode():
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		global_position = GlobalVars.GlobalMousePosition
-		#TODO: Check Waterlayer, if placeable
+		#TODO: Check Navlayer, if placeable
 
 	if event is InputEventMouseButton and event.pressed:
 		if GlobalVars.BuildingMode:
@@ -76,38 +72,3 @@ func _input(event: InputEvent) -> void:
 				GlobalVars.BuildingMode = false
 				get_viewport().set_input_as_handled()
 				pass
-
-
-func _unhandled_key_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
-		GlobalVars.BuildingMode = checkBuildingKey(event.keycode)
-		pass
-
-
-
-func checkBuildingKey(keycode : Key) -> bool:
-	match keycode:
-		KEY_1:
-			print("Key 1 verwendet: Command Center")
-			BuildingToPlace = "COMMAND_CENTER"
-			return true
-		KEY_2:
-			print("Key 2 verwendet: Powerplant")
-			BuildingToPlace = "POWERPLANT"
-			return true
-		KEY_3:
-			print("Key 3 verwendet: Refinery")
-			BuildingToPlace = "REFINERY"
-			return true
-		KEY_4:
-			print("Key 4 verwendet: Barracks")
-			BuildingToPlace = "BARRACKS"
-			return true
-		KEY_5:
-			print("Key 5 verwendet: Radar")
-			BuildingToPlace = "RADAR"
-			return true
-		KEY_ESCAPE:
-			return false
-		_:
-			return false
