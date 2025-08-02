97692209b387f79b0a44fd06ef004b6dc3e16732
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-02 19:36:28 +0200

    Fix: Signal Parameters

--- .src/scripts/globals/signal_bus.gd
+++ .src/scripts/globals/signal_bus.gd
@@ -2,10 +2,10 @@
 class_name GlobalSignalBus extends Node
 
 ## Ausgelöst wenn die Kartegernerierung beendet ist
-signal WORLD_GENERATED
+signal WORLD_GENERATED(_ms:Vector2i, _layer : int)
 
 ## Ausgelöst wenn der Spieler auf dem Spielfeld platziert wurde
-signal PLAYER_RESPAWNED
+signal PLAYER_RESPAWNED(_GlobalPlayePosition : Vector2i)
 
 ## 
 signal BUILDMODE_REQUESTED(requestedbuilding : Dictionary)
@@ -17,11 +17,17 @@ signal BUILDMODE_STRUCTURE_PLACED(structure_iid : int, MousePos : Vector2i)
 signal BUILDMODE_PLACEMENT_POSSIBLE(possible : bool)
 signal BUILDMODE_ERROR(BUILDMODEERROR : String)
 
+
+func _init() -> void:
+	print("GlobalSignalBus => _init()")
+	name = "GlobalSignalBus"
+
+
 func _ready() -> void:
 	print("GlobalSignalBus => _ready()")
 
-	WORLD_GENERATED.connect(func(): pass )
-	PLAYER_RESPAWNED.connect(func(): pass )
+	WORLD_GENERATED.connect(func(_ms, _l): pass )
+	PLAYER_RESPAWNED.connect(func(_p): pass )
 	BUILDMODE_ENTERED.connect(func(): pass )
 	BUILDMODE_LEAVED.connect(func(): pass )
 	BUILDMODE_REQUESTED.connect(func(): pass )
