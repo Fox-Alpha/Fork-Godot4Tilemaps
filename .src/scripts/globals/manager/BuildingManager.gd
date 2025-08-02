3c96ede8f8218d10a2d946b0a44b2ad29bca7ccb
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-02 18:56:08 +0200

    Add: input events Ren: func to check inputkeys Add: signal connect and funcs for buildmode signals Add: sending changed signal if buildselection change in active buildmode ===: To update ghost at cursor Fix: inheritance from ManagerBase

--- .src/scripts/globals/manager/BuildingManager.gd
+++ .src/scripts/globals/manager/BuildingManager.gd
@@ -1,14 +1,14 @@
-class_name BuildingManager extends Node
+class_name BuildingManager extends ManagerBase
 
 
 var BuildingMode : bool :
 	set(value):
-		if BuildingMode != value:
-			BuildingMode = value
-			if value:
-				GlobalVars.GSB.Enter_Buildmode.emit()
-			else:
-				GlobalVars.GSB.Leave_Buildmode.emit()
+		#if BuildingMode != value:
+		BuildingMode = value
+		if value:
+			GlobalVars.GSB.BUILDMODE_ENTERED.emit()
+		else:
+			GlobalVars.GSB.BUILDMODE_LEAVED.emit()
 	get: return BuildingMode
 
 
@@ -48,46 +48,83 @@ var BuildingToPlace : String = ""
 
 
 func _init() -> void:
+	print("BuildingManager => _init()")
+	GlobalVars.GSB.BUILDMODE_REQUESTED.connect(_on_BuildModeRequested)
+	GlobalVars.GSB.BUILDMODE_ENTERED.connect(_On_BuildModeEntered)
+	GlobalVars.GSB.BUILDMODE_LEAVED.connect(_On_BuildModeLeaved)
+	GlobalVars.GSB.BUILDMODE_ERROR.connect(_On_BuildModeError)
 	pass
 
 
 func _ready() -> void:
-	GlobalVars.GSB.REQUEST_BUILD_MODE.connect(_On_BuildMode_Requested)
+	print("BuildingManager => _ready()")
 	pass
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		pass
+
+	if event is InputEventMouseButton and event.pressed:
+		if GlobalVars.BuildingMode:
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				GlobalVars.BuildingMode = false
+				get_viewport().set_input_as_handled()
+				pass
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	pass
+
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
-		GlobalVars.BuildingMode = checkBuildingKey(event.keycode)
+		GlobalVars.BuildingMode = _Check_BuildingKey(event.keycode)
 		pass
 
 
-func checkBuildingKey(keycode : Key) -> bool:
+## Check if pressed key is one of quick building selction
+## Todo: Setting up KeyMapping in ProjectSettings
+func _Check_BuildingKey(keycode : Key) -> bool:
 	match keycode:
 		KEY_1:
 			print("Key 1 verwendet: Command Center")
 			BuildingToPlace = "COMMAND_CENTER"
-			GlobalVars.GSB.REQUEST_BUILD_MODE.emit(Buildings["COMMAND_CENTER"])
+			if BuildingMode:
+				GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.emit(Buildings["COMMAND_CENTER"])
+			else:
+				GlobalVars.GSB.BUILDMODE_REQUESTED.emit(Buildings["COMMAND_CENTER"])
 			return true
 		KEY_2:
 			print("Key 2 verwendet: Powerplant")
 			BuildingToPlace = "POWERPLANT"
-			GlobalVars.GSB.REQUEST_BUILD_MODE.emit(Buildings["POWERPLANT"])
+			if BuildingMode:
+				GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.emit(Buildings["POWERPLANT"])
+			else:
+				GlobalVars.GSB.BUILDMODE_REQUESTED.emit(Buildings["POWERPLANT"])
 			return true
 		KEY_3:
 			print("Key 3 verwendet: Refinery")
 			BuildingToPlace = "REFINERY"
-			GlobalVars.GSB.REQUEST_BUILD_MODE.emit(Buildings["REFINERY"])
+			if BuildingMode:
+				GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.emit(Buildings["REFINERY"])
+			else:
+				GlobalVars.GSB.BUILDMODE_REQUESTED.emit(Buildings["REFINERY"])
 			return true
 		KEY_4:
 			print("Key 4 verwendet: Barracks")
 			BuildingToPlace = "BARRACKS"
-			GlobalVars.GSB.REQUEST_BUILD_MODE.emit(Buildings["BARRACKS"])
+			if BuildingMode:
+				GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.emit(Buildings["BARRACKS"])
+			else:
+				GlobalVars.GSB.BUILDMODE_REQUESTED.emit(Buildings["BARRACKS"])
 			return true
 		KEY_5:
 			print("Key 5 verwendet: Radar")
 			BuildingToPlace = "RADAR"
-			GlobalVars.GSB.REQUEST_BUILD_MODE.emit(Buildings["RADAR"])
+			if BuildingMode:
+				GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.emit(Buildings["RADAR"])
+			else:
+				GlobalVars.GSB.BUILDMODE_REQUESTED.emit(Buildings["RADAR"])
 			return true
 		KEY_ESCAPE:
 			return false
@@ -95,5 +132,18 @@ func checkBuildingKey(keycode : Key) -> bool:
 			return false
 
 
-func _On_BuildMode_Requested(_building : Dictionary) -> void:
+func _on_BuildModeRequested(_building : Dictionary) -> void:
+	pass
+
+
+func _On_BuildModeEntered() -> void:
+	pass
+
+
+func _On_BuildModeLeaved() -> void:
+	pass
+
+
+func _On_BuildModeError(_error : String) -> void:
+	printerr(_error)
 	pass
