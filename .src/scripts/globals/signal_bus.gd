56e00dbc0924f477dbbd92e0bc127e092e9db644
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-02 20:39:51 +0200

    Fix_ pramteres in Dummy func

--- .src/scripts/globals/signal_bus.gd
+++ .src/scripts/globals/signal_bus.gd
@@ -8,14 +8,14 @@ signal WORLD_GENERATED(_ms:Vector2i, _layer : int)
 signal PLAYER_RESPAWNED(_GlobalPlayePosition : Vector2i)
 
 ## 
-signal BUILDMODE_REQUESTED(requestedbuilding : Dictionary)
-signal BUILDMODE_REQUESTED_CHANGED(requestedbuilding : Dictionary)
+signal BUILDMODE_REQUESTED(_requestedbuilding : Dictionary)
+signal BUILDMODE_REQUESTED_CHANGED(_requestedbuilding : Dictionary)
 signal BUILDMODE_ENTERED
 signal BUILDMODE_LEAVED
 
-signal BUILDMODE_STRUCTURE_PLACED(structure_iid : int, MousePos : Vector2i)
-signal BUILDMODE_PLACEMENT_POSSIBLE(possible : bool)
-signal BUILDMODE_ERROR(BUILDMODEERROR : String)
+signal BUILDMODE_STRUCTURE_PLACED(_structure_iid : int, _MousePos : Vector2i)
+signal BUILDMODE_PLACEMENT_POSSIBLE(_possible : bool)
+signal BUILDMODE_ERROR(_BUILDMODEERROR : String)
 
 
 func _init() -> void:
@@ -30,8 +30,8 @@ func _ready() -> void:
 	PLAYER_RESPAWNED.connect(func(_p): pass )
 	BUILDMODE_ENTERED.connect(func(): pass )
 	BUILDMODE_LEAVED.connect(func(): pass )
-	BUILDMODE_REQUESTED.connect(func(): pass )
-	BUILDMODE_REQUESTED_CHANGED.connect(func(): pass )
-	BUILDMODE_PLACEMENT_POSSIBLE.connect(func(): pass )
-	BUILDMODE_STRUCTURE_PLACED.connect(func(): pass )
-	BUILDMODE_ERROR.connect(func(): pass )
+	BUILDMODE_REQUESTED.connect(func(_requestedbuilding : Dictionary): pass )
+	BUILDMODE_REQUESTED_CHANGED.connect(func(_requestedbuilding : Dictionary): pass )
+	BUILDMODE_PLACEMENT_POSSIBLE.connect(func(_possible : bool): pass )
+	BUILDMODE_STRUCTURE_PLACED.connect(func(_structure_iid : int, _MousePos : Vector2i): pass )
+	BUILDMODE_ERROR.connect(func(_BUILDMODEERROR : String): pass )
