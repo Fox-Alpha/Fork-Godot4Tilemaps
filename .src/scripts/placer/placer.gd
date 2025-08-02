4db106d41f5296b9d8a0c91924b83b85afc03d9e
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-02 18:57:07 +0200

    Add: input events Add: signal connect and funcs for buildmode signals Add: receiving changed signal if buildselection change in active buildmode ===: To update ghost at cursor

--- .src/scripts/placer/placer.gd
+++ .src/scripts/placer/placer.gd
@@ -2,59 +2,40 @@ extends Node2D
 
 @onready var build_ghost: Sprite2D = get_node_or_null("BuildGhost")
 
-var placement_possible : bool = true :
+var placement_possible : bool = false :
 	set(value):
 		if placement_possible != value:
 			placement_possible = value
 	get():
 		return placement_possible
 
-
+var BuildingToPlace : Dictionary = {}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GlobalVars.GSB.Enter_Buildmode.connect(EnterBuildMode)
-	GlobalVars.GSB.Leave_Buildmode.connect(LeaveBuildMode)
-	GlobalVars.GSB.Building_Placement_Possible.connect(
+	GlobalVars.GSB.BUILDMODE_ENTERED.connect(_On_BuildModeEntered)
+	GlobalVars.GSB.BUILDMODE_LEAVED.connect(_On_BuildModeLeaved)
+	GlobalVars.GSB.BUILDMODE_PLACEMENT_POSSIBLE.connect(
 	func(_possible : bool):
 		placement_possible = _possible
 		pass
 	)
+	GlobalVars.GSB.BUILDMODE_REQUESTED.connect(_on_BuildModeRequested)
+	GlobalVars.GSB.BUILDMODE_REQUESTED_CHANGED.connect(
+	func(_building : Dictionary):
+		_resetghost()
+		BuildingToPlace = _building
+		)
 	pass # Replace with function body.
 
 
-func _physics_process(delta: float) -> void:
-	HandleBuildMode()
-	pass
-
-
-func _process(_delta: float) -> void:
-	pass
-
-
-func EnterBuildMode() -> void:
-	if GlobalVars.BuildingMode:
-		#var BuildImage : Image = Image.load_from_file(Buildings[BuildingToPlace].Image)
-		#GlobalVars.BuildingToPlace.get_node("Picture").texture
-		#var texture = ImageTexture.create_from_image(BuildImage)
-
-		build_ghost.texture = load(Buildings[BuildingToPlace].Image) as Texture2D
-		build_ghost.show()
-	pass
-
-
-func LeaveBuildMode() -> void:
-	build_ghost.texture = null
-	BuildingToPlace = ""
-	build_ghost.hide()
-	pass
-
-
-func HandleBuildMode():
-	if GlobalVars.GetBuildingMode():
-		#global_position = GlobalVars.GlobalMousePosition
-		pass
+#func _physics_process(_delta: float) -> void:
+	#pass
+#
+#
+#func _process(_delta: float) -> void:
+	#pass
 
 
 func _input(event: InputEvent) -> void:
@@ -65,10 +46,57 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if GlobalVars.BuildingMode:
 			if event.button_index == MOUSE_BUTTON_LEFT:
-				var instance = load(Buildings[BuildingToPlace].Path).instantiate()
-				GlobalVars.GSB.Building_Structure_Placed.emit(instance.get_instance_id(), get_global_mouse_position())
+				if placement_possible:
+					var instance = load(BuildingToPlace.Path).instantiate()
+					GlobalVars.GSB.Building_Structure_Placed.emit(instance.get_instance_id(), get_global_mouse_position())
 				pass
 			if event.button_index == MOUSE_BUTTON_RIGHT:
 				GlobalVars.BuildingMode = false
 				get_viewport().set_input_as_handled()
 				pass
+
+
+#-func _unhandled_input(event: InputEvent) -> void:
+	#pass
+
+
+#func _unhandled_key_input(event: InputEvent) -> void:
+	#pass
+
+
+func _on_BuildModeRequested(_building : Dictionary) -> void:
+	if _building is Dictionary:
+		BuildingToPlace = _building
+	pass
+
+
+func _On_BuildModeEntered() -> void:
+	if GlobalVars.BuildingMode:
+		#var BuildImage : Image = Image.load_from_file(Buildings[BuildingToPlace].Image)
+		#GlobalVars.BuildingToPlace.get_node("Picture").texture
+		#var texture = ImageTexture.create_from_image(BuildImage)
+		var ImgPath = BuildingToPlace.Image
+		# Check Building Image Path
+		if ImgPath.is_empty():
+			GlobalVars.GSB.BUILDMODE_ERROR.emit("Building Imgae is invalid")
+			return
+
+		#
+		build_ghost.texture = load(ImgPath) as Texture2D
+		if build_ghost.texture == null:
+			GlobalVars.GSB.BUILDMODE_ERROR.emit("Building Imgae can not be loaded")
+			return
+
+		build_ghost.show()
+	pass
+
+
+func _On_BuildModeLeaved() -> void:
+	_resetghost()
+
+
+func _resetghost() -> void:
+	build_ghost.hide()
+	build_ghost.texture = null
+	BuildingToPlace = {}
+	pass
