87d43d57a39c64511aeab584df83c4354491ec18
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-02 16:48:39 +0200

    Upd: func and signal name

--- .src/scripts/layer/tree_tile_layer.gd
+++ .src/scripts/layer/tree_tile_layer.gd
@@ -3,10 +3,12 @@ extends TileMapLayerExtension
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GlobalVars.GSB.Building_Structure_Placed.connect(
+	# Remove trees from Map where new Building is placed
+	GlobalVars.GSB.BUILDMODE_STRUCTURE_PLACED.connect(
 		func(_iid:int, coords:Vector2i):
-			AddTileRectToMap(local_to_map(to_local(coords)), 3, false, _TILE_EMPTY)
-			)
+			Add_TileRectToMap(local_to_map(to_local(coords)), 3, false, _TILE_EMPTY)
+			pass	# End Anonymous func
+	) # End connect()
 	pass # Replace with function body.
 
 
