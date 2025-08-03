4ca2c806cb1d1e56f4fea153fbfec462d2b1a026
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-08-03 03:16:57 +0200

    Add: getting Tilemap childs after world has gernated ===: For acceessing e.g. NavigationLayer

--- .src/scripts/layer/layer_group.gd
+++ .src/scripts/layer/layer_group.gd
@@ -1,13 +1,20 @@
 extends Node
 
+var layers : Dictionary
 
 func _ready() -> void:
 	child_entered_tree.connect(_on_child_entered_tree)
+	GlobalVars.GSB.WORLD_GENERATED.connect(func(_s, _l):
+		for c in get_children().filter(func(node):
+			return node is TileMapLayerExtension): # or node is TileMapLayer):
+			layers[c.name] = { "IID" : c.get_instance_id() }
+		pass)
 	pass
 
 
 func _on_child_entered_tree(node: Node) -> void:
-	if node is TileMapLayer:
+	print("LayerGroup: %s has been placed" % [node.name])
+	if node is TileMapLayerExtension:
 		node.owner = self
 		node.unique_name_in_owner = true
 	pass # Replace with function body.
